d607e4f125f625414e947cf160ab1a4f68a7cd96
Bdelg <brian_delgado@brown.edu>
2025-04-16 05:43:32 +0000

    curr iters logging

--- distribution/local/search.js
+++ distribution/local/search.js
@@ -147,6 +147,7 @@ const _processDocs = (scriptOutput, wc) => {
 
 function changeCount(change) {
     currIters += change;  
+    console.log("currIters:", currIters);
 }
 
 function incrementDocumentCount() {
